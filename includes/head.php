fcfa514295c46473082a63b14cfa2b90858fe04b
Hamza <fb.saadi@gmail.com>
2025-04-24 16:01:00 +0100

    Merge remote changes and add my changes3

--- includes/head.php
+++ includes/head.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Barcode System - Your Trusted Partner</title>
+<title>Barcode System - Your Trusted Partner hamza akram morocooo</title>
 <link rel="icon" href="assets\favicon.png" type="image/png">
 <link rel="stylesheet" href="assets/index.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
