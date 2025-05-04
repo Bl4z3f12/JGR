f026fada45a32dbb250331a666534889e4a225b0
Akram <akramfouzi3@gmail.com>
2025-05-04 13:03:01 +0100

    third6

--- includes/head.php
+++ includes/head.php
@@ -9,3 +9,4 @@
 <script src="assets/app.js"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css"/>
